c875dfacd2e41e7ee64b019903ca366b54cb4ac0
Adalberto Sampaio <sampaio.afn@gmail.com>
2022-02-04 14:28:55 -0300

    created mix creation page component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Mix from './pages/Mix';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/mix" element={<Mix />} />
     </Routes>
   );
 }
---
+++ src/pages/Mix.js
@@ -0,0 +1,7 @@
+export default function Mix() {
+  return (
+    <div>
+      <h1>This is the mix creation page!</h1>
+    </div>
+  );
+}
