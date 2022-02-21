436c566c08f71aa57e5150e6a5e33f5ad5b37f04
Adalberto Sampaio <sampaio.afn@gmail.com>
2022-02-21 20:20:08 -0300

    creates a page to list all mixes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Mix from './pages/Mix';
 import { GlobalStyles } from './styles/GlobalStyles';
 import '@fontsource/ibm-plex-sans/400.css';
 import '@fontsource/ibm-plex-sans/500.css';
+import All from './pages/All';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/mix/:name" element={<Mix />} />
+        <Route path="/all" element={<All />} />
       </Routes>
     </>
   );
